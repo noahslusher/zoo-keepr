c6d01068e26d9b0616aecfbdbf58f93341a19730
Carl Wheezer <slushern42@gmail.com>
2022-03-31 18:16:23 -0600

    added id

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const { animals } = require('./data/animals')
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3001;
 const app = express()
 
 function filterByQuery(query, animalsArray) {
@@ -33,12 +33,26 @@ function filterByQuery(query, animalsArray) {
  return filteredResults
 }
 
+function findById(id, animalsArray) {
+ const result = animalsArray.filter(animal => animal.id === id)[0]
+ return result
+}
+
 app.get('/api/animals', (req, res) => {
  let results = animals
  if (req.query) {
   results = filterByQuery(req.query, results)
  }
  res.json(results)
+})
+
+app.get('/api/animals/:id', (req, res) => {
+ const result = findById(req.params.id, animals)
+ if (result) {
+  res.json(result)
+ } else {
+  res.send(404)
+ }
 })
 
 app.listen(PORT, () => {
