59b27498fa321ef32ba3790d7dda98ef13310842
Carl Wheezer <slushern42@gmail.com>
2022-04-05 15:10:54 -0600

    finished 11.3

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(express.urlencoded({ extended: true }))
 //parse incoming json data
 app.use(express.json())
 
+app.use(express.static('public'))
+
 function filterByQuery(query, animalsArray) {
  let personalityTraitsArray = []
  let filteredResults = animalsArray;
@@ -98,6 +100,24 @@ app.post('/api/animals', (req, res) => {
 
   }
 })
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'))
+})
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'))
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
 
 app.listen(PORT, () => {
  console.log(`API server now on port ${PORT}`)
